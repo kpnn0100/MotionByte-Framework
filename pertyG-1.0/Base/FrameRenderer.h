31ae51c93f7f4c15fc19a370e34a6f09fd4bb242
kpnn <110481928+kpnn0100@users.noreply.github.com>
2023-10-31 00:50:24 +0700

    [NC] timer for FrameRenderer

--- pertyG-1.0/Base/FrameRenderer.h
+++ pertyG-1.0/Base/FrameRenderer.h
@@ -1,27 +1,111 @@
 #pragma once
 #include <GLFW/glfw3.h>
 #include <vector>
+#include <iostream>
+#include <memory>
+#include <chrono>
+#include <condition_variable>
+#include <mutex>
 namespace pertyG
 {
-class IFrameEventListener
-{
-public:
-    virtual void onFrameInitialized()
+    class IFrameEventListener
     {
+    public:
+        virtual void onFrameInitialized()
+        {
+        }
+        virtual void onFrameProcessed()
+        {
+        }
+        virtual void onFrameRendered()
+        {
+        }
+    };
+    class FrameRenderer
+    {
+    private:
+        static std::unique_ptr<FrameRenderer> mInstance;
+        std::mutex mEventListenerMutex;
+        std::vector<IFrameEventListener *> mEventListenerList;
+        double mFps;
+        std::thread mTimerThread;
+        bool mTimerRunning;
+        void startTimer()
+        {
+            mTimerRunning = true;
+            mTimerThread = std::thread(&FrameRenderer::framePolling, this);
+        }
+        void framePolling()
+        {
+            while (mTimerRunning)
+            {
+                std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(1000.0 / mFps)));
+                {
+                    std::lock_guard<std::mutex> lock(mEventListenerMutex);
+                    for (auto listener : mEventListenerList)
+                    {
+                        listener->onFrameInitialized();
+                    }
+                    for (auto listener : mEventListenerList)
+                    {
+                        listener->onFrameProcessed();
+                    }
+                    for (auto listener : mEventListenerList)
+                    {
+                        listener->onFrameRendered();
+                    }
+                }
+            }
+        }
+    public:
+        FrameRenderer() : FrameRenderer(60.0)
+        {
+        }
+        FrameRenderer(double fps)
+        {
+            setFps(fps);
+            startTimer();
+        }
 
-    }
-    virtual void onFrameProcessed()
-    {
+        void stopTimer()
+        {
+            mTimerRunning = false;
+            if (mTimerThread.joinable())
+            {
+                mTimerThread.join();
+            }
+        }
 
-    }
-    virtual void onFrameRendered()
-    {
-        
-    }
-};
-class FrameRenderer
-{
-private:
-    static std::vector<IFrameEventListener*> mEventListenerList;
-};
+        static FrameRenderer *instance()
+        {
+            if (!mInstance)
+            {
+                mInstance = std::make_unique<FrameRenderer>();
+            }
+            return mInstance.get();
+        }
+        void setFps(double fps)
+        {
+            mFps = fps;
+        }
+        void addListener(IFrameEventListener *listener)
+        {
+            if (listener == nullptr)
+            {
+                std::cout << "don't regist null pointer" << std::endl;
+                return;
+            }
+            mEventListenerList.push_back(listener);
+        }
+        void removeListener(IFrameEventListener *listener)
+        {
+            for (int i = 0; i < mEventListenerList.size(); i++)
+            {
+                if (mEventListenerList[i] == listener)
+                {
+                    mEventListenerList.erase(mEventListenerList.begin() + i);
+                }
+            }
+        }
+    };
 }
